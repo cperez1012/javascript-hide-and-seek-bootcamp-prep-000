d4e39b70f701e8d00c42a6600c2a519a1960e568
Christian Perez <perezchristian1012@gmail.com>
2019-07-03 01:08:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,5 +10,12 @@ function increaseRankBy(n) {
   parseInt()
 }
 function deepestChild() {
+let node = document.getElementById(`grand-node`)
+let next = node.children [0]
 
+while (next) {
+  node = next
+  next = node.children[0]
+}
+return node
 }
