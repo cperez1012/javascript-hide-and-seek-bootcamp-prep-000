e7e5ec43034d8f90661d6fa76ae8c85d267874b0
Christian Perez <perezchristian1012@gmail.com>
2019-07-03 00:51:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+function getFirstSelector(selector) {
+return document.querySelector(selector)
+}
 
+function nestedTarget() {
+
+}
+
+function increaseRankBy(n) {
+  parseInt()
+}
+function deepestChild() {
+
+}
